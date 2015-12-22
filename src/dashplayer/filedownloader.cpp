cad508d9822cd3b84aaa39d3e38aef8a0e266494
Daniel Posch <daniel.posch@aau.at>
2015-12-22 16:14:35 +0100

    fixed bug for requestRate calculation

--- src/dashplayer/filedownloader.cpp
+++ src/dashplayer/filedownloader.cpp
@@ -2,7 +2,7 @@
 
 using namespace player;
 
-#define BITRATE 800.0 //kbits
+#define BITRATE 1200.0 //kbits
 #define DATA_SIZE 4096.0 //byte
 
 FileDownloader::FileDownloader(int interest_lifetime) : m_face(m_ioService)
@@ -14,7 +14,7 @@ shared_ptr<itec::Buffer> FileDownloader::getFile(string name){
   this->buffer.clear();
   this->buffer.resize (20,chunk());
   this->state = process_state::running;
-  this->requestRate = 1000000/(BITRATE*1000/DATA_SIZE*8); //microseconds
+  this->requestRate = 1000000/((BITRATE*1000)/(DATA_SIZE*8.0)); //microseconds
   this->file = shared_ptr<itec::Buffer>(new itec::Buffer());
   this->file_name = name;
 
@@ -36,7 +36,7 @@ shared_ptr<itec::Buffer> FileDownloader::getFile(string name){
   }
   if(downloadDuration.total_milliseconds () > 0)
   {
-    dwrate = (dwrate * 8) / downloadDuration.total_milliseconds (); //bits/ms
+    dwrate = (dwrate * 8.0) / downloadDuration.total_milliseconds (); //bits/ms
     dwrate *= 1000; //bits/s
   }
   else
